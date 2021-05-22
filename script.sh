d76a765a08b125b83f6d63a4b9630ae296f95ea8
Itachi <70562371+ashwinsutar1996@users.noreply.github.com>
2021-05-22 18:50:07 +0530

    Create script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ echo """### You're running the USERGE-X update script. ###"""
 echo " "
 
 #get username of user
-echo -n "Kakashi or Itachi or gpack repo? (k)/(i)/(g): "
+echo -n "Kakashi or Itachi or gpack or pyrogram repo? (k)/(i)/(g)/(p): "
 read u_n
 
 if [[ "${u_n}" == "k" ]]; then
@@ -34,6 +34,15 @@ if [[ "${u_n}" == "g" ]]; then
         git pull
         git pull https://github.com/code-pms/MyGpack.git
         repo="MyGpack"
+fi
+
+if [[ "${u_n}" == "p" ]]; then
+        rm -rf pyrogram
+        git clone https://github.com/ashwinstr/pyrogram.git
+        cd pyrogram
+        git pull
+        git pull https://github.com/code-rgb/pyrogram.git
+        repo="pyrogram"
 fi
 
 echo " "
